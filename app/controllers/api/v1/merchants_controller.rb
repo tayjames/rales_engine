1310fe00f587cb16dfd046b32f98072efb39d2b0
Tay <tay.james.d@gmail.com>
2019-08-13 18:31:32 -0600

    Updates merchant model to include serializer and params

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    render json: Merchant.all
+    render json: MerchantSerializer.new(Merchant.all)
   end
 
   def show
-    render json: Merchant.find(params[:id])
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
-
-  private
-
-  def merchant_params
-    params.require(:merchant).permit(:name)
-  end
+  # 
+  # private
+  #
+  # def merchant_params
+  #   params.require(:merchant).permit(:name)
+  # end
 end
