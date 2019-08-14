d08fe7eb0cad5f5758385a107669edd5b16bf966
Tay <tay.james.d@gmail.com>
2019-08-13 18:30:16 -0600

    Adds namespaced routes to Merchants with find,find_all,random

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/find', to: 'find#show'
+        get '/find_all', to: 'find#index'
+        get '/random', to: 'random#show'
+      end
       resources :merchants, only: [:index, :show]
     end
   end
