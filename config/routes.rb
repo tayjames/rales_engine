5d044451afc32e4d9ff478343ac574f0f32a1d69
Tay <tay.james.d@gmail.com>
2019-08-15 14:09:34 -0600

    Adds WIP routes -- finishes merchants,items,invoices

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,18 @@ Rails.application.routes.draw do
         get '/find_all', to: 'find#index'
         get '/random', to: 'random#show'
       end
+
+      namespace :items do
+        get '/find', to: 'find#show'
+        get '/find_all', to: 'find#index'
+        get '/random', to: 'random#show'
+      end
+
+      namespace :invoices do
+        get '/find', to: 'find#show'
+        get '/find_all', to: 'find#index'
+        get '/random', to: 'random#show'
+      end
       resources :merchants, only: [:index, :show]
       resources :items, only: [:index, :show]
       resources :customers, only: [:index, :show]
